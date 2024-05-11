b838dceb7f8787e4a10fe4d1cc3ba234daa4fad9
hanjunnn <qwedfr79@naver.com>
2024-05-11 11:14:37 +0900

    코드 오류수정

--- src/main/java/com/example/edu/controller/NewsSearchController.java
+++ src/main/java/com/example/edu/controller/NewsSearchController.java
@@ -1,6 +1,7 @@
 package com.example.edu.controller;
 
 import com.example.edu.model.NewsInfoVO;
+import com.example.edu.service.NewsSearchService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,7 +15,7 @@ import java.util.Map;
 public class NewsSearchController{
 
     @Resource(name = "newsSearchService")
-    private newsSearchService newsSearchService;
+    private NewsSearchService newsSearchService;
 
     @RequestMapping(value = "/news")
     public String newsSearchPage() throws Exception {
