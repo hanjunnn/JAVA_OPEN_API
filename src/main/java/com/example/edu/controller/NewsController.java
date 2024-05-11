a8a2ccba55d37d3e207a3a8bff9ff0817b3e3295
hanjunnn <qwedfr79@naver.com>
2024-05-11 15:51:28 +0900

    스크랩후 뒤로가기 생성

--- src/main/java/com/example/edu/controller/NewsController.java
+++ src/main/java/com/example/edu/controller/NewsController.java
@@ -3,9 +3,13 @@ package com.example.edu.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import com.example.edu.repository.NewsRepository; // NewsRepository를 가져옵니다.
 import com.example.edu.entity.EntityNews;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 
 
@@ -17,11 +21,12 @@ public class NewsController {
 
     @PostMapping("/scrap")
     //@ResponseBody
-    public String scrapNews(@RequestParam Map<String,Object> news) {
+    public ResponseEntity<String> scrapNews(@RequestParam Map<String,Object> news, HttpServletResponse response) throws IOException {
         EntityNews entityNews = mapToEntity(news);
         newsRepository.save(entityNews);
-        return "success";
+        return ResponseEntity.status(HttpStatus.OK).body("스크랩 되었습니다!"+"<br><a href='javascript:history.back()'>뒤로가기</a>");
     }
+
     private EntityNews mapToEntity(Map<String, Object> news) {
         EntityNews entityNews = new EntityNews();
         entityNews.setTitle((String) news.get("title"));
