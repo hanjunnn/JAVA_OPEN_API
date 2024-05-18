5c442c064cb4e9aa6fe719ce18786393c3bcb348
hanjunnn <qwedfr79@naver.com>
2024-05-18 09:14:34 +0900

    삭제버튼 추가

--- src/main/java/com/example/edu/controller/ScrapController.java
+++ src/main/java/com/example/edu/controller/ScrapController.java
@@ -1,7 +1,9 @@
 package com.example.edu.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.edu.repository.NewsRepository;
 import com.example.edu.entity.EntityNews;
@@ -18,8 +20,14 @@ public class ScrapController {
     public String getAllNews(Model model) {
         List<EntityNews> newsList = newsRepository.findAll();
         model.addAttribute("newsList", newsList);
-        newsList.forEach(x->System.out.println(x.getLink()));
+        newsList.forEach(x -> System.out.println(x.getLink()));
 
         return "scrapall"; // scrap.jsp 파일의 이름
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteNews(@PathVariable("id") Long id) {
+        newsRepository.deleteById(id);
+        return "redirect:/scraplist";
     }
 }
